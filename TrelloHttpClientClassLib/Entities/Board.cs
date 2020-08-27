b302f2ed86d11f1c82d37f879f2d0a894480011c
Igor Garbuz <kinetikapotencia@gmail.com>
2020-08-28 01:22:38 +0300

    fix spaces

--- TrelloHttpClientClassLib/Entities/Board.cs
+++ TrelloHttpClientClassLib/Entities/Board.cs
@@ -3,8 +3,11 @@
     public class Board
     {
         public string Id { get; set; }
+        
         public string Name { get; set; }
+        
         public string Desc { get; set; }
+        
         public string Url { get; set; }
     }
 }
