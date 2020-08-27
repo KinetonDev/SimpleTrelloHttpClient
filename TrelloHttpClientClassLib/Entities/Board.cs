6c7287fef2c3d4fc716561e9d1b99fb3d85dd83f
Igor Garbuz <kinetikapotencia@gmail.com>
2020-08-28 01:16:40 +0300

    init

---
+++ TrelloHttpClientClassLib/Entities/Board.cs
@@ -0,0 +1,10 @@
+﻿namespace TrelloHttpClientClassLib.Entities
+{
+    public class Board
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ TrelloHttpClientClassLib/Entities/CardsList.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace TrelloHttpClientClassLib.Entities
+{
+    public class CardsList
+    {
+        public string Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        [JsonProperty("idBoard")]
+        public string BoardId { get; set; }
+    }
+}
